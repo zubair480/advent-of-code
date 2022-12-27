a8939d95f9571f6ac2f16e7f15a99124a07c8814
Zubair Zafar <zubairzafar480@gmail.com>
2022-12-27 08:53:34 +0500

    14

--- 14.py
+++ 14.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python3
+
 import sys
 import math
 from copy import deepcopy
 from collections import defaultdict, deque
-infile = sys.argv[1] if len(sys.argv)>1 else '14.in'
+infile = sys.argv[1] if len(sys.argv)>1 else 'day14.txt'
 data = open(infile).read().strip()
 lines = [x for x in data.split('\n')]
 
