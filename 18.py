432062fa8b66acaf4085b93b01b768e40b35ebf9
Zubair Zafar <zubairzafar480@gmail.com>
2022-12-27 08:57:33 +0500

    18

--- 18.py
+++ 18.py
@@ -3,7 +3,7 @@ import sys
 import math
 from copy import deepcopy
 from collections import defaultdict, deque
-infile = sys.argv[1] if len(sys.argv)>1 else '18.in'
+infile = sys.argv[1] if len(sys.argv)>1 else 'day18.txt'
 data = open(infile).read().strip()
 lines = [x for x in data.split('\n')]
 
