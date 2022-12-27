fc365334d8b527da6711db56b94d71bf3e8969a0
Zubair Zafar <zubairzafar480@gmail.com>
2022-12-27 08:54:02 +0500

    15

--- 15.py
+++ 15.py
@@ -3,7 +3,7 @@ import sys
 import math
 from copy import deepcopy
 from collections import defaultdict, deque
-infile = sys.argv[1] if len(sys.argv)>1 else '15.in'
+infile = sys.argv[1] if len(sys.argv)>1 else 'day15.txt'
 data = open(infile).read().strip()
 lines = [x for x in data.split('\n')]
 
