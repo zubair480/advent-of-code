f067754a30d1966d06d496d1363eba97e9531685
Zubair Zafar <zubairzafar480@gmail.com>
2022-12-27 09:20:02 +0500

    24

--- 24.py
+++ 24.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python3
+
 import sys
 from decimal import Decimal
 import math
 from copy import deepcopy
 from collections import defaultdict, deque
-infile = sys.argv[1] if len(sys.argv)>1 else '24.in'
+infile = sys.argv[1] if len(sys.argv)>1 else 'day24.txt'
 data = open(infile).read().strip()
 lines = [x for x in data.split('\n')]
 
